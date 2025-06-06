63bec32a731b8be584e3ddccc2de7e74d5ad68b9
Ryth00 <josevan47106@gmail.com>
2025-06-06 22:49:37 +0700

    update: tambah gambar & music

---
+++ js/playlist.js
@@ -0,0 +1,69 @@
+
+let songs = [
+  {
+    name: "Love Yourself",
+    artist: "Justin Bieber",
+    img: "icon1",
+    audio: "loveyourself",
+  },
+  {
+    name: "Where We Are",
+    artist: "One Direction",
+    img: "icon2",
+    audio: "Where We Are",
+  },
+  {
+    name: "Blue",
+    artist: "YungKai",
+    img: "icon3",
+    audio: "blue",
+  },
+  {
+    name: "Perfect",
+    artist: "Ed Sheeran",
+    img: "icon4",
+    audio: "perfect",
+  },
+
+  {
+    name: "Favorite Lesson",
+    artist: "Yaeow",
+    img: "icon10",
+    audio: "favorite lesson",
+  },
+
+  {
+    name: "The Lazy Song",
+    artist: "Bruno Mars",
+    img: "icon5",
+    audio: "lazysong",
+  },
+
+  {
+    name: "Tabun",
+    artist: "YOASOBI",
+    img: "icon7",
+    audio: "tabun",
+  },
+
+  {
+    name: "KOIIRO",
+    artist: "MOSAWO",
+    img: "icon9",
+    audio: "koiiro",
+  },
+
+  {
+    name: "Yori Ni Kakeru",
+    artist: "YOASOBI",
+    img: "icon8",
+    audio: "yori ni kakeru",
+  },
+
+  {
+    name: "Gyutto",
+    artist: "MOSAWO",
+    img: "icon6",
+    audio: "gyutto",
+  },
+];
